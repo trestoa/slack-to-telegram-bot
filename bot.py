a7fa6ee4852952918f76c15a501df220c481f5c5
Markus Klein <m@mklein.co.at>
2016-11-14 20:53:33 +0100

    add bot

---
+++ bot.py
@@ -0,0 +1,41 @@
+import time
+
+from slackclient import SlackClient
+from telegram.bot import Bot
+
+SLACK_TOKEN = ''
+sc = SlackClient(SLACK_TOKEN)
+
+TELEGRAM_TOKEN = ''
+TELEGRAM_TARGET = ''
+telegram_bot = Bot(TELEGRAM_TOKEN)
+
+if sc.rtm_connect():
+    while True:
+        messages = sc.rtm_read()
+        for message in messages:
+            try:
+                if message['type'] == 'message':
+                    if message['channel'][0] == 'G':
+                        channel = sc.api_call(
+                            "groups.info",
+                            channel=message['channel'])['group']
+                    elif message['channel'][0] == 'C':
+                        channel = sc.api_call(
+                            "channels.info",
+                            channel=message['channel'])['channel']
+                    else:
+                        channel = {'name': 'bot'}
+
+                    user = sc.api_call(
+                        "users.info",
+                        user=message['user'])['user']
+
+                    msg_string = '@{} posted to #{}: {}'.format(user['name'], channel['name'], message['text'])
+                    telegram_bot.sendMessage(TELEGRAM_TARGET, msg_string)
+            except e:
+                print('Could not send message.')
+                print(e)
+        time.sleep(1)
+else:
+    print("Connection Failed, invalid token?")
